39f96e4ef4d4f0108e7780bddead64230e3a6624
Luthfan123 <54345954+Luthfan123@users.noreply.github.com>
2021-05-04 13:53:30 +0530

    Add files via upload

---
+++ arithematic.java
@@ -0,0 +1,39 @@
+class arithematic{
+
+public static void main(String args[]) {
+
+ int a=50,b=35;//declaring int varibles and assigning values
+ 
+          
+ 
+  System.out.println("VALUE OF first number : "+a);
+
+  System.out.println("VALUE OF second number : "+b);
+
+  System.out.println(a + " + " + b + " = " + (a+b));
+
+  System.out.println(a + " - " + b + " = " + (a-b));
+
+  System.out.println(a + " * " + b + " = " + (a*b));
+
+  System.out.println(a + " / " + b + " = " + (a/b));	
+
+}
+
+}
+
+
+
+/*
+   OUTPUT
+ C:\Users\USER\java_pgms>javac arithematic.java
+
+C:\Users\USER\java_pgms>java arithematic
+VALUE OF first number : 50
+VALUE OF second number : 35
+50 + 35 = 85
+50 - 35 = 15
+50 * 35 = 1750
+50 / 35 = 1 
+ 
+*/

10ce83d08dae3d58f6aa9eedd6c914bcf6566c62
Luthfan123 <54345954+Luthfan123@users.noreply.github.com>
2021-05-04 13:20:55 +0530

    Add files via upload

---
+++ variables.java
@@ -0,0 +1,25 @@
+class variables {
+          
+           public static void main(String args[]){
+                  int num;
+                  num=100;
+           
+                  System.out.println("this is num:"+num);
+                  num=num*2;
+                  System.out.print("the value of num*2 is:");
+                  System.out.println(num);
+          }
+}
+
+                
+
+
+
+
+                   OUTPUT
+
+  C:\Users\USER\java_pgms>javac variables.java
+
+C:\Users\USER\java_pgms>java variables
+this is num:100
+the value of num*2 is:200
